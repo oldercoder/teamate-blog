8874f3a9af67e090b2ecb8e276cc6e09740a6763
oldercoder <zengyu.jerry@gmail.com>
2014-03-04 14:18:49 +0800

    add post function

---
+++ models/post.js
@@ -0,0 +1,74 @@
+var mongodb = require('./db');
+
+function Post(name, title, post){
+    this.name = name;
+    this.title = title;
+    this.post = post;
+}
+module.exports = User;
+
+Post.prototype.save = function save(callback){
+    var date = new Date();
+    var time = {
+        date: date,
+        year: date.getFullYear(),
+        month: date.getFullYear() + '-' + (date.getMonth() +1),
+        day: date.getFullYear() + '-' + (date.getMonth() +1) + '-' + date.getDate(),
+        minute: date.getFullYear() + '-' + (date.getMonth() +1) + '-' + date.getDate() +''+
+            date.getHours() + ':' + (date.getMinutes()<10? '0' + date.getMinutes():date.getMinutes())
+    };
+
+    var post = {
+        name: this.name,
+        time: time,
+        title: this.title,
+        post: this.post
+    };
+
+    // open database
+    mongodb.open(function(err, db){
+        if(err){
+            return callback(err)
+        }
+        //read posts collection
+        db.collection('posts', function (err, collection){
+            if(err){
+                mongodb.close();
+                return callback(err);
+            }
+            //insert doc into collection
+            collection.ensureIndex('user');
+            collection.insert(post, {safe:true}, function(err, post){
+                mongodb.close();
+                callback(err, post);
+           });
+        });
+    });
+};
+
+Post.get = function get(username, callback){
+    mongodb.open(function(err, db){
+        if(err){
+            return callback(err)
+        }
+        //read collection
+        db.collection('posts', function(err, collection){
+            if(err){
+                mongodb.close();
+                return callback(err);
+            }
+            //query doc with 'user=username', if null means all
+            var query={};
+            if(username){
+                query.user = username;
+            }
+            collection.find(query).sort({time: -1}).toArray(function (err, docs){
+                mongodb.close();
+                if(err){
+                    return callback(err, null);
+                }
+                callback(null, docs);
+            });
+        });
+    });
+};
